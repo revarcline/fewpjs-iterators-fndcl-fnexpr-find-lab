f6890ece5b870bb40322d1beaa7ba2d9a00b737b
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-25 16:38:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+function superbowlWin(record) {
+  // record looks like array of objects, values are strings:
+  // [ {year: '2018', result: 'L'} ]
+  let result = record.find((season) => season.result === "W");
+  return result ? result.year : undefined;
+}
